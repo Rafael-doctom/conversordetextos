1a840340446a2abd00c0d716d29a36f7d60aa511
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2024-05-20 17:30:56 -0300

    feat: inicial commit & features added

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+let txt = document.querySelector("#floatingTextarea2");
+let btn_copy = document.querySelector("#copy");
+let responseInner = document.querySelector("#response");
+let btnClear = document.querySelector("#clear");
+let capitalLetters = document.querySelector('#capital-letters');
+let smallLetters = document.querySelector('#small-letters');
+
+
+// copiar texto
+btn_copy.addEventListener('click', (e) => {
+    txt.select();
+    document.execCommand("copy");
+    e.target.focus();
+
+    if (txt.value !== '') {
+        responseInner.innerHTML = ` ${txt.value}`;
+
+
+    } else {
+        responseInner.innerHTML = 'Caixa vazia!';
+    }
+
+});
+
+// LETRAS MAIÚSCULAS
+capitalLetters.addEventListener('click', () => {
+
+    let letter = txt.value.toUpperCase();
+    txt.value = letter
+    responseInner.innerHTML = `${letter}`;
+
+});
+
+// small letter
+smallLetters.addEventListener('click', () => {
+
+    let letter = txt.value.toLowerCase();
+    txt.value = letter
+    responseInner.innerHTML = `${letter}`;
+
+});
+
+
+// Limpar texto
+btnClear.addEventListener('click', () => {
+    if (txt.value !== '') {
+        txt.value = '';
+        responseInner.innerHTML = '';
+    }
+});

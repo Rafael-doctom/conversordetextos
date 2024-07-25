6c8ceea3b3073aa18fb7b65bf788d10a83c79d68
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2024-07-25 11:32:42 -0300

    feat: function to reverse texts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let txt = document.querySelector("#floatingTextarea2");
     let responseInner = document.querySelector("#response");
-    let mainContent = document.querySelector(".main-content"); 
+    let mainContent = document.querySelector(".main-content");
 
     mainContent.addEventListener('click', (e) => {
         if (e.target.matches('#copy')) {
@@ -9,29 +9,52 @@ document.addEventListener('DOMContentLoaded', () => {
             document.execCommand("copy");
             e.target.focus();
             responseInner.innerHTML = txt.value !== '' ? txt.value : 'Caixa vazia!';
-        } else if (e.target.matches('#capital-letters')) {
+        }
+        else if (e.target.matches('#capital-letters')) {
             let letter = txt.value.toUpperCase();
             txt.value = letter;
             responseInner.innerHTML = txt.value;
-        } else if (e.target.matches('#small-letters')) {
+        }
+
+
+        else if (e.target.matches('#small-letters')) {
             let letter = txt.value.toLowerCase();
             txt.value = letter;
             responseInner.innerHTML = txt.value;
-        } else if (e.target.matches('#qtd-length')) {
-            responseInner.innerHTML = `Quantidade de Caracteres: ${txt.value.length}`;
-        } else if (e.target.matches('#remove-spaces')) {
+        }
+
+
+        else if (e.target.matches('#qtd-length')) {
+            let removeSpaces = txt.value.replace(/\s+/g, '');
+            responseInner.innerHTML = `Quantidade de Caracteres: ${removeSpaces.length}`;
+        }
+
+        else if (e.target.matches('#invert-text')) {
+
+            let invertTxt = txt.value.split('').reverse().join('')
+
+            responseInner.innerHTML = `${invertTxt}`;
+        }
+
+        else if (e.target.matches('#remove-spaces')) {
             let newText = txt.value.replace(/\s+/g, ' ').trim();
             txt.value = newText;
             responseInner.innerHTML = 'Espaços extras removidos!';
-        } else if (e.target.matches('#remove-spaces-all')) {
+        }
+
+        else if (e.target.matches('#remove-spaces-all')) {
             let noSpacesText = txt.value.replace(/\s+/g, '');
             txt.value = noSpacesText;
             responseInner.innerHTML = 'Todos os espaços foram removidos!';
-        } else if (e.target.matches('#capitalize-words')) {
+        }
+
+        else if (e.target.matches('#capitalize-words')) {
             let capitalizedText = txt.value.replace(/\b\w/g, char => char.toUpperCase());
             txt.value = capitalizedText;
             responseInner.innerHTML = 'Início de cada palavra capitalizado!';
-        } else if (e.target.matches('#clear')) {
+        }
+
+        else if (e.target.matches('#clear')) {
             if (txt.value !== '') {
                 txt.value = '';
                 responseInner.innerHTML = '';
@@ -39,3 +62,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
